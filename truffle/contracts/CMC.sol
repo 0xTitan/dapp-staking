dea1964ad84e4bef3d9f027336444e1f89e5e086
0xTitan <clement.suzat@gmail.com>
2022-07-27 22:46:22 +0200

    Add mint functionnality on token + test

--- truffle/contracts/CMC.sol
+++ truffle/contracts/CMC.sol
@@ -3,9 +3,28 @@
 pragma solidity 0.8.14;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract CMC is ERC20 {
-    constructor(uint256 initialSupply) ERC20("CAMACL", "CMC") {
+    uint256 private maxTotalSupply = 10000000;
+
+    constructor(uint256 initialSupply) ERC20("CAMACLE", "CMC") {
+        require(
+            initialSupply <= maxTotalSupply,
+            "The total supply for CMC token will be exceed"
+        );
         _mint(msg.sender, initialSupply);
+    }
+
+    //allow users to call the requestTokens function to mint tokens
+    function mint(uint256 amount) external {
+        require(amount <= 1000, "Max claimable token is 1000");
+        require(
+            IERC20(address(this)).totalSupply() + amount <= maxTotalSupply,
+            "The total supply for CMC token will be exceed"
+        );
+
+        //mint tokens
+        _mint(msg.sender, amount);
     }
 }
--- truffle/test/CMC.test.js
+++ truffle/test/CMC.test.js
@@ -8,7 +8,7 @@ const {
 const { expect } = require("chai");
 const CMC = artifacts.require("CMC");
 contract("CMC", function (accounts) {
-  const _name = "CAMACL";
+  const _name = "CAMACLE";
   const _symbol = "CMC";
   const _initialsupply = new BN(1000);
   const _decimals = new BN(18);
@@ -16,108 +16,146 @@ contract("CMC", function (accounts) {
   const recipient = accounts[1];
   const spender = accounts[2];
 
-  beforeEach(async function () {
-    this.CMCInstance = await CMC.new(_initialsupply, { from: owner });
-  });
-
-  it("a un nom", async function () {
-    expect(await this.CMCInstance.name()).to.equal(_name);
-  });
-  it("a un symbole", async function () {
-    expect(await this.CMCInstance.symbol()).to.equal(_symbol);
-  });
-  it("a une valeur décimal", async function () {
-    expect(await this.CMCInstance.decimals()).to.be.bignumber.equal(_decimals);
-  });
-  it("vérifie la balance du propriétaire du contrat", async function () {
-    let balanceOwner = await this.CMCInstance.balanceOf(owner);
-    let totalSupply = await this.CMCInstance.totalSupply();
-    expect(balanceOwner).to.be.bignumber.equal(totalSupply);
-  });
-  it("vérifie si un transfer est bien effectué", async function () {
-    let balanceOwnerBeforeTransfer = await this.CMCInstance.balanceOf(owner);
-    let balanceRecipientBeforeTransfer = await this.CMCInstance.balanceOf(
-      recipient
-    );
-    let amount = new BN(10);
-
-    await this.CMCInstance.transfer(recipient, amount, { from: owner });
-    let balanceOwnerAfterTransfer = await this.CMCInstance.balanceOf(owner);
-    let balanceRecipientAfterTransfer = await this.CMCInstance.balanceOf(
-      recipient
-    );
-
-    expect(balanceOwnerAfterTransfer).to.be.bignumber.equal(
-      balanceOwnerBeforeTransfer.sub(amount)
-    );
-    expect(balanceRecipientAfterTransfer).to.be.bignumber.equal(
-      balanceRecipientBeforeTransfer.add(amount)
-    );
-  });
-  it("vérifie si une adresse est approuve", async function () {
-    let approvedAmountBefore = await this.CMCInstance.allowance(owner, spender);
-    let amount = new BN(1);
-    let isApproved = await this.CMCInstance.approve(spender, amount, {
-      from: owner,
+  describe("TOKEN CREATION AND TRANSFER", () => {
+    beforeEach(async function () {
+      this.CMCInstance = await CMC.new(_initialsupply, { from: owner });
+      await this.CMCInstance.mint(_initialsupply);
     });
-    expectEvent(isApproved, "Approval", {
-      owner: owner,
-      spender: spender,
-      value: amount,
-    });
-    let approvedAmount = await this.CMCInstance.allowance(owner, spender);
 
-    expect(approvedAmount).to.be.bignumber.equal(
-      approvedAmountBefore.add(amount)
-    );
-  });
-  it("vérifie si une adresse à pu faire un transfer", async function () {
-    /*The use-case is that you give permission for someone else to transfer from your account (contract in that case).
+    it("a un nom", async function () {
+      expect(await this.CMCInstance.name()).to.equal(_name);
+    });
+    it("a un symbole", async function () {
+      expect(await this.CMCInstance.symbol()).to.equal(_symbol);
+    });
+    it("a une valeur décimal", async function () {
+      expect(await this.CMCInstance.decimals()).to.be.bignumber.equal(
+        _decimals
+      );
+    });
+    it("vérifie la balance du propriétaire du contrat", async function () {
+      let balanceOwner = await this.CMCInstance.balanceOf(owner);
+      let totalSupply = await this.CMCInstance.totalSupply();
+      expect(balanceOwner).to.be.bignumber.equal(totalSupply);
+    });
+    it("vérifie si un transfer est bien effectué", async function () {
+      let balanceOwnerBeforeTransfer = await this.CMCInstance.balanceOf(owner);
+      let balanceRecipientBeforeTransfer = await this.CMCInstance.balanceOf(
+        recipient
+      );
+      let amount = new BN(10);
+
+      await this.CMCInstance.transfer(recipient, amount, { from: owner });
+      let balanceOwnerAfterTransfer = await this.CMCInstance.balanceOf(owner);
+      let balanceRecipientAfterTransfer = await this.CMCInstance.balanceOf(
+        recipient
+      );
+
+      expect(balanceOwnerAfterTransfer).to.be.bignumber.equal(
+        balanceOwnerBeforeTransfer.sub(amount)
+      );
+      expect(balanceRecipientAfterTransfer).to.be.bignumber.equal(
+        balanceRecipientBeforeTransfer.add(amount)
+      );
+    });
+    it("vérifie si une adresse est approuve", async function () {
+      let approvedAmountBefore = await this.CMCInstance.allowance(
+        owner,
+        spender
+      );
+      let amount = new BN(1);
+      let isApproved = await this.CMCInstance.approve(spender, amount, {
+        from: owner,
+      });
+      expectEvent(isApproved, "Approval", {
+        owner: owner,
+        spender: spender,
+        value: amount,
+      });
+      let approvedAmount = await this.CMCInstance.allowance(owner, spender);
+
+      expect(approvedAmount).to.be.bignumber.equal(
+        approvedAmountBefore.add(amount)
+      );
+    });
+    it("vérifie si une adresse à pu faire un transfer", async function () {
+      /*The use-case is that you give permission for someone else to transfer from your account (contract in that case).
       The owner give the permission to spender account to transfer token from the contract.
       Then spender can use the function transferFrom once it's approved*/
 
-    let balanceOwnerBeforeTransfer = await this.CMCInstance.balanceOf(owner);
-    let balanceRecipientBeforeTransfer = await this.CMCInstance.balanceOf(
-      recipient
-    );
-    let amount = new BN(1);
-    await this.CMCInstance.approve(spender, amount, {
-      from: owner,
-    });
-    await this.CMCInstance.transferFrom(owner, recipient, amount, {
-      from: spender,
-    });
-    let balanceOwnerBeforeAfter = await this.CMCInstance.balanceOf(owner);
-    let balanceRecipientAfter = await this.CMCInstance.balanceOf(recipient);
-
-    expect(balanceOwnerBeforeAfter).to.be.bignumber.equal(
-      balanceOwnerBeforeTransfer.sub(amount)
-    );
-
-    expect(balanceRecipientAfter).to.be.bignumber.equal(
-      balanceRecipientBeforeTransfer.add(amount)
-    );
-  });
-  it("vérifie si une adresse à son transfer refusé si le montant dépasse ce qui a été approuvé", async function () {
-    /*The use-case is that you give permission for someone else to transfer from your account (contract in that case).
-      The owner give the permission to spender account to transfer token from the contract.
-      Then spender can use the function transferFrom once it's approved*/
-
-    let balanceOwnerBeforeTransfer = await this.CMCInstance.balanceOf(owner);
-    let balanceRecipientBeforeTransfer = await this.CMCInstance.balanceOf(
-      recipient
-    );
-    let amount = new BN(1);
-    let overAmount = new BN(2);
-    await this.CMCInstance.approve(spender, amount, {
-      from: owner,
-    });
-
-    await expectRevert(
-      this.CMCInstance.transferFrom(owner, recipient, overAmount, {
+      let balanceOwnerBeforeTransfer = await this.CMCInstance.balanceOf(owner);
+      let balanceRecipientBeforeTransfer = await this.CMCInstance.balanceOf(
+        recipient
+      );
+      let amount = new BN(1);
+      await this.CMCInstance.approve(spender, amount, {
+        from: owner,
+      });
+      await this.CMCInstance.transferFrom(owner, recipient, amount, {
         from: spender,
-      }),
-      "ERC20: insufficient allowance"
-    );
+      });
+      let balanceOwnerBeforeAfter = await this.CMCInstance.balanceOf(owner);
+      let balanceRecipientAfter = await this.CMCInstance.balanceOf(recipient);
+
+      expect(balanceOwnerBeforeAfter).to.be.bignumber.equal(
+        balanceOwnerBeforeTransfer.sub(amount)
+      );
+
+      expect(balanceRecipientAfter).to.be.bignumber.equal(
+        balanceRecipientBeforeTransfer.add(amount)
+      );
+    });
+    it("vérifie si une adresse à son transfer refusé si le montant dépasse ce qui a été approuvé", async function () {
+      /*The use-case is that you give permission for someone else to transfer from your account (contract in that case).
+      The owner give the permission to spender account to transfer token from the contract.
+      Then spender can use the function transferFrom once it's approved*/
+
+      let balanceOwnerBeforeTransfer = await this.CMCInstance.balanceOf(owner);
+      let balanceRecipientBeforeTransfer = await this.CMCInstance.balanceOf(
+        recipient
+      );
+      let amount = new BN(1);
+      let overAmount = new BN(2);
+      await this.CMCInstance.approve(spender, amount, {
+        from: owner,
+      });
+
+      await expectRevert(
+        this.CMCInstance.transferFrom(owner, recipient, overAmount, {
+          from: spender,
+        }),
+        "ERC20: insufficient allowance"
+      );
+    });
+  });
+  context("MINT", () => {
+    before(async function () {
+      this.CMCInstance = await CMC.new(9999000, { from: owner });
+    });
+
+    it("verifie si une adresse peut minter", async function () {
+      await this.CMCInstance.mint(_initialsupply, { from: recipient });
+      let balanceRecipient = await this.CMCInstance.balanceOf(recipient);
+      //let totalSupply = await this.CMCInstance.totalSupply();
+      expect(balanceRecipient).to.be.bignumber.equal(_initialsupply);
+    });
+    it("verifie si une adresse à son mint refusée si la quantité est dépassé", async function () {
+      await expectRevert(
+        this.CMCInstance.mint(_initialsupply + 1, { from: recipient }),
+        "Max claimable token is 1000"
+      );
+    });
+    it("verifie si que l'init du contrat ne depasse pas le total supply", async function () {
+      await expectRevert(
+        CMC.new(1000000000000000, { from: owner }),
+        "The total supply for CMC token will be exceed"
+      );
+    });
+    it("verifie si une adresse à son mint refusé si la total suply est dépassée", async function () {
+      await expectRevert(
+        this.CMCInstance.mint(_initialsupply, { from: recipient }),
+        "The total supply for CMC token will be exceed"
+      );
+    });
   });
 });
