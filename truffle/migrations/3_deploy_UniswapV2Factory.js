82aac802fb71bb48a44c67b15e36b3bd47a24247
mo7tr <mo7tr@gmail.com>
2022-08-02 18:40:14 +0200

    factory instantiated with creation of the first CMC FETH pair on local testnet, must finish to create CMC pair on other live testnet with their own eth address

--- truffle/migrations/3_deploy_UniswapV2Factory.js
+++ truffle/migrations/3_deploy_UniswapV2Factory.js
@@ -2,6 +2,7 @@ const json = require("@uniswap/v2-core/build/UniswapV2Factory.json");
 const contract = require("@truffle/contract");
 const UniswapV2Factory = contract(json);
 
+const CMC = artifacts.require("CMC");
 const FETH = artifacts.require("FETH");
 
 UniswapV2Factory.setProvider(web3._provider);
@@ -19,13 +20,39 @@ module.exports = async function (deployer, network, accounts) {
 
   console.log(3);
 
+  const CMCInstance = await CMC.deployed();
+  console.log("CMC address =>", CMCInstance.address);
+
+  console.log(4);
+
   if (network === "development") {
-    console.log(33, "development");
+    console.log(5, "development start");
+
     await deployer.deploy(FETH);
     const FETHInstance = await FETH.deployed();
     console.log("FETH address =>", FETHInstance.address);
     await FETHInstance.faucet(accounts[0], 1000);
+
+    const createPair = await UniswapV2FactoryInstance.createPair(
+      FETHInstance.address,
+      CMCInstance.address,
+      { from: accounts[0] }
+    );
+    console.log("createPair =>", createPair);
+
+    const pair0 = await UniswapV2FactoryInstance.allPairs(0);
+    console.log("pair0 =>", pair0);
+
+    console.log(5, "development finish");
+  } else if (network === "ropsten") {
+    console.log(5, "ropsten start");
+    // to complement
+    console.log(5, "ropsten finish");
+  } else if (network === "kovan") {
+    console.log(5, "kovan start");
+    // to complement
+    console.log(5, "kovan finish");
   }
 
-  console.log(4);
+  console.log(6);
 };
