c3a1ea6b7aaa88109f875f6329012c072df36239
mo7tr <mo7tr@gmail.com>
2022-08-03 15:09:23 +0200

    add constructor in CMCliquidity contract with router and factory address + tailoring of the 3rd deployment script

--- truffle/contracts/CMCLiquidity.sol
+++ truffle/contracts/CMCLiquidity.sol
@@ -10,25 +10,31 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 /** Staking contract with reward for CMC token */
 contract CMCLiquidity is Ownable {
     address private constant WETH = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
+    address public router;
+    address public factory;
 
     event Log(string message, uint256 val);
+
+    constructor(address _router, address _factory) {
+        router = _router;
+        factory = _factory;
+    }
 
     function addLiquidity(
         address _tokenA,
         address _tokenB,
         uint256 _amountA,
-        uint256 _amountB,
-        address _router
+        uint256 _amountB
     ) external {
         //transfer both token to smart contract
         IERC20(_tokenA).transferFrom(msg.sender, address(this), _amountA);
         IERC20(_tokenB).transferFrom(msg.sender, address(this), _amountB);
 
         //approve router address to allow token usage
-        IERC20(_tokenA).approve(_router, _amountA);
-        IERC20(_tokenB).approve(_router, _amountB);
+        IERC20(_tokenA).approve(router, _amountA);
+        IERC20(_tokenB).approve(router, _amountB);
         (uint256 amountA, uint256 amountB, uint256 liquidity) = IUniswapV2Router02(
-            _router
+            router
         ).addLiquidity(
                 _tokenA,
                 _tokenB,
@@ -45,18 +51,13 @@ contract CMCLiquidity is Ownable {
         emit Log("liquitity", liquidity);
     }
 
-    function removeLiquidity(
-        address _tokenA,
-        address _tokenB,
-        address _factory,
-        address _router
-    ) external {
-        address pair = IUniswapV2Factory(_factory).getPair(_tokenA, _tokenB);
+    function removeLiquidity(address _tokenA, address _tokenB) external {
+        address pair = IUniswapV2Factory(factory).getPair(_tokenA, _tokenB);
         uint256 liquidity = IERC20(pair).balanceOf(address(this));
 
-        IERC20(pair).approve(_router, liquidity);
+        IERC20(pair).approve(router, liquidity);
 
-        (uint256 amountA, uint256 amountB) = IUniswapV2Router02(_router)
+        (uint256 amountA, uint256 amountB) = IUniswapV2Router02(router)
             .removeLiquidity(
                 _tokenA,
                 _tokenB,
--- truffle/migrations/3_deploy_UniswapV2Factory.js
+++ truffle/migrations/3_deploy_UniswapV2Factory.js
@@ -44,7 +44,11 @@ module.exports = async function (deployer, network, accounts) {
   );
   console.log(4);
 
-  await deployer.deploy(CMCLiquidity);
+  await deployer.deploy(
+    CMCLiquidity,
+    UniswapV2RouterInstance.address,
+    UniswapV2FactoryInstance.address
+  );
   const CMCLiquidityInstance = await CMCLiquidity.deployed();
   console.log("CMCLiquidityInstance address =>", CMCLiquidityInstance.address);
 
@@ -53,12 +57,14 @@ module.exports = async function (deployer, network, accounts) {
 
   console.log(5);
 
+  //////////////////////////////
+
   if (network === "development") {
     console.log(6, "development start");
 
     await deployer.deploy(FETH);
     const FETHInstance = await FETH.deployed();
-    console.log("FETH address =>", FETHInstance.address);
+    console.log(6, "FETH address =>", FETHInstance.address);
     let decimals = web3.utils.toBN(18);
     let mintedQtyToOwner = web3.utils.toBN(1000);
     await FETHInstance.faucet(
@@ -66,46 +72,53 @@ module.exports = async function (deployer, network, accounts) {
       mintedQtyToOwner.mul(web3.utils.toBN(10).pow(decimals))
     );
 
+    console.log(
+      6,
+      "balance owner FETH =>",
+      (await FETHInstance.balanceOf(accounts[0])).toString()
+    );
+
     const createPair = await UniswapV2FactoryInstance.createPair(
       FETHInstance.address,
       CMCInstance.address,
       { from: accounts[0] }
     );
-    //console.log("createPair =>", createPair);
+    console.log(6, "createPair.logs =>", createPair.logs);
 
     const pair0 = await UniswapV2FactoryInstance.allPairs(0);
-    console.log("pair0 =>", pair0);
+    console.log(6, "pair0 =>", pair0);
 
     //approve CMCLiquidity
-    let feth = new web3.eth.Contract(ERC20.abi, FETHInstance.address);
+    // let feth = new web3.eth.Contract(ERC20.abi, FETHInstance.address);
     await FETHInstance.approve(CMCLiquidityInstance.address, 10000);
-    console.log("approved CMCLiquidityInstance for FETH");
+    console.log(6, "approved CMCLiquidityInstance for FETH");
 
     await CMCInstance.approve(CMCLiquidityInstance.address, 10000);
-    console.log("approved CMCLiquidityInstance for CMC");
+    console.log(6, "approved CMCLiquidityInstance for CMC");
 
     let newPair = new web3.eth.Contract(ERC20.abi, pair0);
-     await newPair.methods
-       .approve(CMCLiquidityInstance.address, 10000)
+    await newPair.methods
+      .approve(CMCLiquidityInstance.address, 10000)
       .send({ from: accounts[0] });
+
+    console.log(6, "newPair =>", newPair);
 
     const lp = await CMCLiquidityInstance.addLiquidity(
       FETHInstance.address,
       CMCInstance.address,
       10000,
-      10000,
-      UniswapV2RouterInstance.address
+      10000
     );
 
-    console.log(lp);
+    console.log(6, "lp.logs =>", lp.logs);
 
     const pairBalance = await newPair.methods.balanceOf(
       CMCLiquidityInstance.address
     );
 
-    console.log("pairBalance : " + pairBalance);
+    console.log(6, "pairBalance : " + pairBalance);
 
-    console.log(7, "development finish");
+    console.log(6, "development finish");
   } else if (network === "ropsten") {
     console.log(5, "ropsten start");
     // to complement
@@ -116,5 +129,5 @@ module.exports = async function (deployer, network, accounts) {
     console.log(5, "kovan finish");
   }
 
-  console.log(6);
+  console.log(7);
 };
